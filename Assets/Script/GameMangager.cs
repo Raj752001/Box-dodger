9ba573c856fc59070f8058168db8b1acf85e1cc6
Raj752001 <u19cs011@coed.svnit.ac.in>
2021-04-04 09:28:10 +0530

    update

--- Assets/Script/GameMangager.cs
+++ Assets/Script/GameMangager.cs
@@ -9,18 +9,30 @@ public class GameMangager : MonoBehaviour
     public GameObject mainCanvas;
     public Text mainScoreText;
     public GameObject gameOverCanvas;
+    public Image pausePlayButtonImage;
+    public Sprite pauseImage;
+    public Sprite playImage;
+    public Image pausePlayAudioImage;
+    public Sprite pauseAudio;
+    public Sprite playAudio;
+    public GameObject pauseBackgroundImage;
     public Text gameOverScoreText;
+    public Text highScoreText;
     public AudioSource mainAudioSource;
     public AudioClip mainAudioClip;
     public AudioClip gameOverAudioClip;
 
     bool isGameOver=false;
+    bool ispause = false;
+    bool isPauseAudio = false;
 
     // Start is called before the first frame update
     void Start()
     {
+
         mainCanvas.SetActive(true);
         gameOverCanvas.SetActive(false);
+
         FindObjectOfType<PlayerControl>().OnPlayerDeath += OnGameOver;
     }
 
@@ -52,6 +64,49 @@ public class GameMangager : MonoBehaviour
         AudioSource.PlayClipAtPoint(gameOverAudioClip, transform.position);
         mainCanvas.SetActive(false);
         gameOverCanvas.SetActive(true);
+        float highScore = PlayerPrefs.GetFloat("HighScore", 0);
+        if (Time.timeSinceLevelLoad > highScore)
+        {
+            highScore = Time.timeSinceLevelLoad;
+            PlayerPrefs.SetFloat("HighScore", highScore);
+        }
+        highScoreText.text = "High Socre : " + Mathf.Round(highScore).ToString();
         gameOverScoreText.text = Mathf.Round(Time.timeSinceLevelLoad).ToString();
+    }
+
+    public void PausePlayGame()
+    {
+        if (!ispause)
+        {
+            pausePlayButtonImage.sprite = pauseImage;
+            pauseBackgroundImage.SetActive(true);
+            ispause = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pausePlayButtonImage.sprite = playImage;
+            pauseBackgroundImage.SetActive(false);
+            ispause = false;
+            Time.timeScale = 1;
+        }
+        
+    }
+
+    public void PlayMuteAudio()
+    {
+        if (!isPauseAudio)
+        {
+            pausePlayAudioImage.sprite = pauseAudio;
+            isPauseAudio = true;
+            mainAudioSource.Stop();
+        }
+        else
+        {
+            pausePlayAudioImage.sprite = playAudio;
+            isPauseAudio = false;
+            mainAudioSource.Play();
+        }
+
     }
 }
